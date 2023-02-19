d731603eec6bde69260e821e437e56bccd855ce3
Ivanou Uladzislau <vladislavivanov440@gmail.com>
2023-02-19 23:12:20 +0300

    add: config folder

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,33 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  console.log(options);
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes(".module."),
+            localIdentName: options.isDev
+              ? "[path][name]__[local]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoader, cssLoader];
+}
